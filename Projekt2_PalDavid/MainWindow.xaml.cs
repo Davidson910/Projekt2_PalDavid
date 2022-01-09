4dde131ddc85c7b8431f554a0559d5f0f894abe9
Davidson910 <paldavid.0910@gmail.com>
2022-01-09 19:31:52 +0100

    Mennek a csigák

--- Projekt2_PalDavid/MainWindow.xaml.cs
+++ Projekt2_PalDavid/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Projekt2_PalDavid
 {
@@ -20,9 +21,29 @@ namespace Projekt2_PalDavid
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer idozito;
+        int hatar = 13;
+        int hatar2 = 23;
+
         public MainWindow()
         {
             InitializeComponent();
+            idozito = new DispatcherTimer();
+            idozito.Interval = TimeSpan.FromSeconds(1);
+            idozito.Tick += new EventHandler(Csigahaladas);
+        }
+
+        private void Csigahaladas(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            csiga1.Margin = new Thickness(hatar += rnd.Next(10, 30), 54, 0, 0);
+            csiga2.Margin = new Thickness(hatar += rnd.Next(10, 30), 232, 0, 0);
+            csiga3.Margin = new Thickness(hatar2 += rnd.Next(10, 30), 417, 0, 0);
+
+        }
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            idozito.Start();
         }
     }
 }
